d3eec70e93feb55f944025457dc652677b034efa
20079934 <20079934@mail.wit.ie>
2021-03-11 16:26:52 +0000

    Cards now display

--- app/src/main/java/com/w20079934/activities/Diary.kt
+++ app/src/main/java/com/w20079934/activities/Diary.kt
@@ -6,12 +6,24 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.w20079934.adapters.EntryAdapter
+import com.w20079934.adapters.EntryListener
+import com.w20079934.main.DiaryApp
+import com.w20079934.models.EntryModel
 import com.w20079934.mydiary_2.R
+import kotlinx.android.synthetic.main.activity_diary.*
 
-class Diary : AppCompatActivity() {
+class Diary : AppCompatActivity(), EntryListener {
+    lateinit var app: DiaryApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
+
+        app = this.application as DiaryApp
+        recyclerView.setLayoutManager(LinearLayoutManager(this))
+        recyclerView.adapter = EntryAdapter(app.entries.findAll(), this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -27,9 +39,13 @@ class Diary : AppCompatActivity() {
                 true
             }
             R.id.diaryRename -> {
-                Toast.makeText(this, "Not implemented yet", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, getString(R.string.feature_notImplemented), Toast.LENGTH_LONG).show()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onEntryClick(entry: EntryModel) {
+        Toast.makeText(this, getString(R.string.feature_notImplemented), Toast.LENGTH_SHORT).show()
     }
 }
