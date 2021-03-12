e4eda750332fa83a2f3a3832cc7448a4a5a54be5
20079934 <20079934@mail.wit.ie>
2021-03-12 15:39:11 +0000

    Navigating between the two fragments functional

--- app/src/main/java/com/w20079934/activities/Home.kt
+++ app/src/main/java/com/w20079934/activities/Home.kt
@@ -7,8 +7,10 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
+import com.w20079934.fragments.DiaryFragment
 import com.w20079934.fragments.EntryFragment
 import com.w20079934.mydiary_2.R
 import kotlinx.android.synthetic.main.app_bar_home.*
@@ -37,7 +39,7 @@ class Home : AppCompatActivity(),
 
         ft = supportFragmentManager.beginTransaction()
 
-        val fragment = EntryFragment.newInstance()
+        val fragment = DiaryFragment.newInstance()
         ft.replace(R.id.homeFrame, fragment)
         ft.commit()
     }
@@ -45,12 +47,19 @@ class Home : AppCompatActivity(),
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
         when (item.itemId) {
-            R.id.nav_newEntry -> Toast.makeText(this, "You Selected new entry", Toast.LENGTH_SHORT).show()
-
+            R.id.nav_Diary -> navigateTo(DiaryFragment.newInstance())
+            R.id.nav_newEntry -> navigateTo(EntryFragment.newInstance())
             else -> Toast.makeText(this, getString(R.string.feature_notImplemented), Toast.LENGTH_SHORT).show()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun navigateTo(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.homeFrame, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/w20079934/fragments/EntryFragment.kt
+++ app/src/main/java/com/w20079934/fragments/EntryFragment.kt
@@ -5,12 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.w20079934.helpers.readImageFromPath
+import com.w20079934.helpers.showImagePicker
+import com.w20079934.main.DiaryApp
+import com.w20079934.models.EntryModel
 import com.w20079934.mydiary_2.R
+import kotlinx.android.synthetic.main.fragment_entry.*
+import kotlinx.android.synthetic.main.fragment_entry.view.*
 
 class EntryFragment : Fragment() {
 
+    lateinit var app : DiaryApp
+    var entry = EntryModel()
+    var edit = false
+    val IMAGE_REQUEST=1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = activity?.application as DiaryApp
+
+
+
     }
 
     override fun onCreateView(
@@ -20,6 +37,43 @@ class EntryFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_entry, container, false)
         activity?.title = getString(R.string.menu_new_entry)
+
+        if(activity?.intent?.hasExtra("entry_edit") == true) {
+            edit = true
+            entry = activity?.intent?.extras?.getParcelable<EntryModel>("entry_edit")!!
+            root.entryTopic.setText(entry.topic)
+            root.entryEntry.setText(entry.entry)
+            if (entry.image != "")
+            {
+                root.entryImage.setImageBitmap(readImageFromPath(activity!!, entry.image))
+                root.chooseImage.text = getString(R.string.button_changeImage)
+            }
+        }
+
+
+        root.chooseImage.setOnClickListener {
+            showImagePicker(activity!!, IMAGE_REQUEST)
+        }
+        root.btnAdd.setOnClickListener()
+        {
+            if (entryEntry.text.isNotEmpty()) {
+                if(edit) {
+                    entry.topic = entryTopic.text.toString()
+                    entry.entry = entryEntry.text.toString()
+                    app.entries.update(entry)
+
+                } else {
+                    entry.topic = entryTopic.text.toString()
+                    entry.entry = entryEntry.text.toString()
+                    app.entries.create(entry.copy())
+                }
+
+
+            } else {
+                Toast.makeText(activity!!, getString(R.string.menu_invalidEntry), Toast.LENGTH_LONG).show()
+            }
+        }
+
 
         return root;
     }
@@ -31,4 +85,6 @@ class EntryFragment : Fragment() {
                     arguments = Bundle().apply {}
                 }
     }
+
+
 }
